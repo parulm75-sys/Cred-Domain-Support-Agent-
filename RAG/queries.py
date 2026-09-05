24da1c10f09028c7bdd085b0bdad95c343aba5fd
parulm75-sys <parulm75@gmail.com>
2026-09-05 17:00:46 -0400

    Queries file updated and added 12 in scope queries and 2 out of scope

---
+++ RAG/queries.py
@@ -0,0 +1,57 @@
+QUERIES = [{
+    "doc_id":"account_closure_process",
+    "query":"How many days it will take to close my account?",
+    "in_scope": True
+},{
+    "doc_id":"credit_card_fee_structure",
+    "query":"What is the annual fee for the credit card?",
+    "in_scope": True
+},{
+    "doc_id":"credit_score_impact_factors",
+    "query":"How can I increase my credit score?",
+    "in_scope": True
+},{
+    "doc_id":"EMI_calculation_rules",
+    "query":"What would be my EMI if I take a loan of 50000 INR at 1% rate monthly for 70 months",
+    "in_scope": True
+},{
+    "doc_id":"fraud_dispute_resolution_process",
+    "query":"I think there is a transaction that is unknown to me, what can I do?",
+    "in_scope": True
+},{
+    "doc_id":"interest_rate_slabs",
+    "query":"I have taken the loan of 76567 INR, what is my interest rate?",
+    "in_scope": True
+},{
+    "doc_id":"joint_account_rules",
+    "query":"me and my wife holds a joint account, I want to make a withdrawal, can I come by myself?",
+    "in_scope": True
+},{
+    "doc_id":"KYC_document_requirements",
+    "query":"MY KYC fails, what is my next step?",
+    "in_scope": True
+},{
+    "doc_id":"loan_eligibility_criteria_by_loan_type",
+    "query":"I want to apply for Car Loan and I have credit score of 701, am I eligible to apply?",
+    "in_scope": True
+},{
+    "doc_id":"minimum_balance_requirements",
+    "query":"Why I am being charged 100 INR monthly, my account balance is 799 INR for now?",
+    "in_scope": True
+},{
+    "doc_id":"NRI_account_eligibility",
+    "query":"What are the documents required to apply for NRI account?",
+    "in_scope": True
+},{
+    "doc_id":"prepayment_penalty_rules",
+    "query":"What is the fees, I am looking at if I want to pay my 60000 INR loan after the first year?",
+    "in_scope": True
+},{
+    "doc_id":None,
+    "query":"What is the monthly salary of the bank employee",
+    "in_scope": False
+},{
+    "doc_id":None,
+    "query":"What is the capital of India?",
+    "in_scope": False
+}]
