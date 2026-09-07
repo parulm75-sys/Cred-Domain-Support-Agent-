a5fc5c0eeaa3533dda2625c2abe02e11f8cc71a8
parulm75-sys <parulm75@gmail.com>
2026-09-06 22:21:09 -0400

    coded LangGraph agent and added notes about in Read.md file

---
+++ agent/graph.py
@@ -0,0 +1,58 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph, END
+from RAG.retrieve import generate, sen_collection
+from agent.tools import check_loan_application_status
+import re
+class AgentState(TypedDict):
+    query: str
+    intent: str
+    result: dict
+    response: str
+def classify(state):
+    check=state["query"].lower()
+    policy=["status","application"]
+    if(policy[0] in check or policy[1] in check):
+        return{"intent":"record"}
+    else:
+        return{"intent":"policy"}
+def policy(state):
+    return {"result":generate(state["query"],sen_collection)}
+def record(state):
+    match = re.search(r"\d+", state["query"])
+    if(match!=None):
+        number=match.group()
+        return {"result":check_loan_application_status(number)}
+    else:
+        return{"result":None} 
+def format_response(state):
+    result=state["result"]
+    intent=state["intent"]
+    text=""
+    if(result==None):
+        return{"response": "Sorry, we could not find any record"}
+    if(intent=="policy"):
+        text="You can find your answer below:\n"+"\n".join(state["result"]["content"])
+    else:
+        text=f"The status of the record is {result['status']}, the outstanding loan amount is {result['loan_amount_inr']} and escalation required: "
+        if(result["escalation_score"]>=0.5):
+            text+="Yes"
+        else:
+            text+="No"
+    return {"response": text}
+def route(state):
+    if state["intent"] == "record":
+        return "record"
+    return "policy"
+graph = StateGraph(AgentState)
+graph.set_entry_point("classify")
+graph.add_node("classify", classify)
+graph.add_node("policy", policy)
+graph.add_node("record", record)
+graph.add_node("format_response", format_response)
+graph.add_edge("policy", "format_response")
+graph.add_edge("record", "format_response")
+graph.add_edge("format_response", END)
+graph.add_conditional_edges("classify", route)
+app = graph.compile()
+print(app.invoke({"query": "What is the annual fee for the credit card?"}))
+print(app.invoke({"query": "What is the status of application 23?"}))
